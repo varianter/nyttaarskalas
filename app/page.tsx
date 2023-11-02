1966310a1c5d6b10626771a64be77995b511a6c4
Jonas Brunvoll Larsson <jbl@variant.no>
2023-11-02 12:15:04 +0100

    Updating variable name and components in tailwindCSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-24">
-      <h1 className="mb-3 text-6xl font-semibold">Nyttårskalas🎆</h1>
+      <h1 className="mb-3 font-semibold">Nyttårskalas🎆</h1>
     </main>
   );
 }
--- app/program/page.tsx
+++ app/program/page.tsx
@@ -1,11 +1,11 @@
 export default function Program() {
   const program = [
-    { activety: "Åpningstale", clock: "19.00" },
-    { activety: "Minling", clock: "19.10" },
-    { activety: "Dumdum boys", clock: "19.30" },
-    { activety: "Gorillaz", clock: "20.30" },
-    { activety: "DDE", clock: "21.30" },
-    { activety: "Rævkrokturnering", clock: "23.30" },
+    { name: "Åpningstale", clock: "19.00" },
+    { name: "Minling", clock: "19.10" },
+    { name: "Dumdum boys", clock: "19.30" },
+    { name: "Gorillaz", clock: "20.30" },
+    { name: "DDE", clock: "21.30" },
+    { name: "Rævkrokturnering", clock: "23.30" },
   ];
 
   return (
@@ -13,9 +13,9 @@ export default function Program() {
       <h3 className="mb-2 text-3xl font-semibold  pt-20 pb-10">
         Kveldens program
       </h3>
-      {program.map((x, index) => (
+      {program.map((activety, index) => (
         <div key={index} className="text-xl py-2">
-          {x.clock} - {x.activety}
+          {activety.clock} - {activety.name}
         </div>
       ))}
     </main>
